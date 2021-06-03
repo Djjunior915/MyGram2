e3a38ae50dd6367e1c9e4c9dfdfb55464c8133ba
Rafael Hernandez <rhernandez7071@southwestuniversity.edu>
2021-06-03 17:36:36 -0600

    GitHub assignment module 4

---
+++ MyClasses/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyGram.MyClasses
+{
+    public class Roles
+    {
+        public Roles()
+        {
+        }
+
+        public int RoleId { get; set; }
+        public string Rolename { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MyGram.MyClasses;
+
+
+namespace MyGram
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(2021,6,3);
+            DateTime myDateTimeVariable2 = DateTime.Now;
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
