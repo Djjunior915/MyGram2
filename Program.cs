bd2742f8dba31ce291036b580325eb32046a0ccc
Rafael Hernandez <rhernandez7071@southwestuniversity.edu>
2021-06-10 11:16:33 -0600

    Assignment 5

--- Program.cs
+++ Program.cs
@@ -9,23 +9,15 @@ namespace MyGram
     {
         static void Main()
         {
-            DateTime myFirstDateVariable = new DateTime(2021, 6, 10);
-            DateTime mySecondDateVariable = new DateTime(2010, 6, 10);
+            int result = 0;
 
-            int result = DateTime.Compare(myFirstDateVariable, mySecondDateVariable);
-
-            //1 = First date is greater than the secomd
-            //0 = Means the two dates are equal
-            //-1 = First date is not greater than the second
-
-            if (result == 1)
+            for(int x=1;x<21;x++) //3
             {
-                Console.WriteLine("First date is greater");
-            } else
-            {
-                Console.WriteLine("Second date is greater");
+                result = result + x; //6
 
             }
+
+            Console.WriteLine(result);
         }
     }
 }
